8c0cf5cd098a462b599f6fac9a97be6170348b39
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-09-30 19:42:28 +0800

    seeder for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','prp_assigned'
+        'rold_id', 'name', 'email', 'password','prp_assigned'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('role_id')->index();
+            $table->unsignedBigInteger('role_id')->index()->default(5);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
