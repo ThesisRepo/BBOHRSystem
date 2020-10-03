c19f26dba27e50137c39e07013dc5bd71123d1ed
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-10-03 17:45:37 +0800

    dashboard unit test

---
+++ app/Http/Controllers/Employees/RegularEmployeeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Employees;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegularEmployeeController extends Controller
+{
+    //
+}
