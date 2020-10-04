d6e218a4681b59c2b2b1b79a9ffad6ec806e4f0d
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-10-04 10:19:29 +0800

    ha

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -7,16 +7,5 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
-    /**
-     * 
-     * @test
-     * 
-     * A basic test example.
-     * 
-     * @return void
-     */
-    public function only_logged_in_users_can_see_the_dashboard()
-    {
-        $response = $this->get('/home')->assertRedirect('/login');
-    }
+    
 }
