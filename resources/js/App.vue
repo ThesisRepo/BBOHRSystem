a75cb45a5bcc554a9d752ad4472f29cf2732f66a
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-10-09 23:09:17 +0800

    solved issue of undefined name of property non-object

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -24,7 +24,7 @@ export default {
     },
     methods: {
         setUserType(){
-            localStorage.setItem('user_type', this.user.role.name)
+            localStorage.setItem('user_type', this.user.roles[0].name)
         }
     }
 }
