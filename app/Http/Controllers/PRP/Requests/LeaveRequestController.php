16ebb7a03ff786795676dcfc70409b9b6fec4831
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-10-26 16:53:56 +0800

    shift change request can be approved'

--- app/Http/Controllers/PRP/Requests/LeaveRequestController.php
+++ app/Http/Controllers/PRP/Requests/LeaveRequestController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers\PRP\Requests;
 
 use Illuminate\Http\Request;
 use App\Eloquent\Implementations\Requests\LeaveRequestEloquent;
-use App\Http\Controllers\Controller;
 use App\Services\ApproverRequestService;
-use App\Models\Requests\LeaveRequest;
 
 class LeaveRequestController extends RequestBaseController
 {
     protected $leave_request;
-    protected $request_service;
 
     public function __construct(
         LeaveRequestEloquent $leave_request,
@@ -20,19 +17,12 @@ class LeaveRequestController extends RequestBaseController
 
         $this->middleware(['auth', 'verify.prp']);  
         $this->leave_request = $leave_request;
-        $this->request_service = $request_service;
-        $this->request_service->setRequestType('leave_request');
+        parent::__construct($request_service,  $leave_request, 'leave_request');
     }
     
     public function getPendingRequest($id){
 
-        $user = $this->request_service->getUserRoles($id);
-        $max_role = $this->request_service->getMaxRoles($user->roles);
-        if($max_role == 2){
-            $res = $this->leave_request->getPendingRequestForApprover($id, $max_role);
-        }else {
-            $res = $this->leave_request->getPendingRequestForApproverAdmin($id, $max_role);            
-        }
+        $res = $this->getBasePendingRequest($id);
 
         return $res;
 
@@ -40,54 +30,33 @@ class LeaveRequestController extends RequestBaseController
 
     public function getAllFeedbackedRequest($user_id){
 
-        return $this->request_service->getAllFeedbackedRequest($user_id, 'feedbacked_leave_requests');
-    
+        $res = $this->getBaseAllFeedbackedRequest($user_id, 'feedbacked_leave_requests');
+        
+        return $res;
+
     }
 
     public function getAllApprovedRequest($user_id){
 
-        $user = $this->request_service->getUserRoles($user_id);
-        $max_role = $this->request_service->getMaxRoles($user->roles);
-        if($max_role == 5 ) {
+        $res = $this->getBaseAllApprovedRequest($user_id, 'feedbacked_leave_requests');
 
-            return $this->request_service->getAllApprovedRequest($user_id, 'feedbacked_leave_requests', true);
-
-        }
-        return $this->request_service->getAllApprovedRequest($user_id, 'feedbacked_leave_requests');
+        return $res;
     
     }
 
     public function getAllDisapprovedRequest($user_id){
 
-        return $this->request_service->getAllDisapprovedRequest($user_id, 'feedbacked_leave_requests');
-    
+        $res = $this->getBaseAllDisapprovedRequest($user_id, 'feedbacked_leave_requests');
+        
+        return $res;
+
     }
 
     public function requestFeedback($id, Request $request){
 
-        $data = [];
-        $user = $this->request_service->getUserRoles($request->user_id);
-        $max_role = $this->request_service->getMaxRoles($user->roles);
-        $current_request = $this->leave_request->find($id);   
-        $next_approver = $this->request_service->getNextApprover($max_role);
-        if($current_request->approver_role_id == $max_role || ($current_request->approver_role_id == $next_approver && $current_request->status_id == 1)) {
-            if($request->status_id == 2) {
-                if($max_role != 5) {
-                    $data['approver_role_id'] = $next_approver;
-                }else {
-                    $data['status_id'] = $request->status_id;
-                }
-                $res = $this->leave_request->approveRequest($id, $data, $current_request, $user);
-            }else if($request->status_id == 3){
-                $data['status_id'] = $request->status_id;
-                $data['approver_role_id'] = $max_role;
-                $res = $this->leave_request->disapproveRequest($id, $data, $current_request, $user);
-            }
-            return $res;
-        }else {
-            return response()->json(['message'=> 'don\'t have permission to make feedback on this request'], 400);
-        } 
+        $res =requestBaseFeedback($id, $request);
 
-
+        return $res;
+        
     }
 }
--- routes/users/prp.php
+++ routes/users/prp.php
@@ -1,16 +1,42 @@
 <?php
 
+// -- LEAVE REQUEST
+
+// user_id:4,
+// status_id:3,
+
 // get all feedbacked leave request that belongs of approver
-Route::get('prp/leave_request/feedbacked/{user_id}','Requests\LeaveRequestController@getAllFeedbackedRequest')->name('prp.get_all_feedbacked_leave_request');
+Route::get('prp/leave_request/feedbacked/{user_id}','Requests\LeaveRequestController@getAllFeedbackedRequest')->name('prp.leave_request.feedbacked');
 
 // get all approved leave request that belongs of approver
-Route::get('prp/leave_request/approved/{user_id}','Requests\LeaveRequestController@getAllApprovedRequest')->name('prp.get_all_approved_leave_request');
+Route::get('prp/leave_request/approved/{user_id}','Requests\LeaveRequestController@getAllApprovedRequest')->name('prp.leave_request.approved');
 
 // get all disapproved leave request that belongs of approver
-Route::get('prp/leave_request/disapproved/{user_id}','Requests\LeaveRequestController@getAllDisapprovedRequest')->name('prp.get_all_disapproved_leave_request');
+Route::get('prp/leave_request/disapproved/{user_id}','Requests\LeaveRequestController@getAllDisapprovedRequest')->name('prp.leave_request.disapproved');
 
 // get all pending leave request that belongs of approver
-Route::get('prp/leave_request/pending/{user_id}','Requests\LeaveRequestController@getPendingRequest')->name('leave_request.get_pending_request_to_approve');
+Route::get('prp/leave_request/pending/{user_id}','Requests\LeaveRequestController@getPendingRequest')->name('prp.leave_request.pending');
 
 // approve or disapprove a leave request
-Route::post('prp/leave_request/feedback/{id}','Requests\LeaveRequestController@requestFeedback')->name('leave_request.feedback');
+Route::post('prp/leave_request/feedback/{id}','Requests\LeaveRequestController@requestFeedback')->name('prp.leave_request.feedback');
+
+
+// -- SHIFT CHANGE REQUEST
+
+// user_id:4,
+// status_id:3,
+
+// get all feedbacked shift change request that belongs of approver
+Route::get('prp/shift_change_request/feedbacked/{user_id}','Requests\ShiftChangeRequestController@getAllFeedbackedRequest')->name('prp.shift_change_request.feedbacked');
+
+// get all approved shift change request that belongs of approver
+Route::get('prp/shift_change_request/approved/{user_id}','Requests\ShiftChangeRequestController@getAllApprovedRequest')->name('prp.shift_change_request.approved');
+
+// get all disapproved lshift changeeave request that belongs of approver
+Route::get('prp/shift_change_request/disapproved/{user_id}','Requests\ShiftChangeRequestController@getAllDisapprovedRequest')->name('prp.shift_change_request.disapproved');
+
+// get all pending shift change request that belongs of approver
+Route::get('prp/shift_change_request/pending/{user_id}','Requests\ShiftChangeRequestController@getPendingRequest')->name('prp.shift_change_request.pending');
+
+// approve or disapprove a shift change request
+Route::post('prp/shift_change_request/feedback/{id}','Requests\ShiftChangeRequestController@requestFeedback')->name('prp.shift_change_request.feedback');
