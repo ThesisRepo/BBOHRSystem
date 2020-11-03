f685ae4c3e925f14c928026f6b7d9865ba538f73
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-04 07:54:13 +0800

    retrieve of all user accounts and unfinished user seeder

---
+++ app/Traits/EmployeeInfoList.php
@@ -0,0 +1,287 @@
+<?php
+namespace App\Traits;
+use Hash;
+
+trait EmployeeInfoList
+{
+  public $employee_info_list = [
+        [
+          'user' => [
+            'prp_assigned' => 3,
+            'finance_assigned' => 4,
+            'first_name' => 'Cielo Fe Sasing',
+            'last_name' => 'Atis',
+            'email' => 'Cielofe@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 1
+            'company_number'=> '1234652'
+            // 'profile_url'=> null
+            'company_position'=> 'Customer Support Leader'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1,2]
+        ],
+        [
+          'user' => [
+            'prp_assigned' => 3,
+            'finance_assigned' => 4,
+            'first_name' => 'Gerard Winston Villegas',
+            'last_name' => 'Hamoy',
+            'email' => 'GerardHamoy@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 4
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'Apps Developer'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1,2]
+        ],
+        [
+          'user' => [
+            'prp_assigned' => 3,
+            'finance_assigned' => 4,
+            'first_name' => 'Brisbane Jr Real',
+            'last_name' => 'Bacalla',
+            'email' => 'BrisbaneBacalla@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 5
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'PHP DEV'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1,2]
+        ],
+        [
+          'user' => [
+            // 'prp_assigned' => 3,
+            // 'finance_assigned' => 4,
+            'first_name' => 'Dianen',
+            'last_name' => 'Gemarino',
+            'email' => 'DianenGemarino@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'MKTG. Assistant'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1]
+        ],
+     
+        [
+          'user' => [
+            // 'prp_assigned' => 3,
+            // 'finance_assigned' => 4,
+            'first_name' => 'Nina Rose Autida',
+            'last_name' => 'Lapiz',
+            'email' => 'NinaLapiz@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'Sales & Support'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1]
+        ],
+        [
+          'user' => [
+            // 'prp_assigned' => 3,
+            // 'finance_assigned' => 4,
+            'first_name' => 'Jocel Redotco',
+            'last_name' => 'Mendoza',
+            'email' => 'JocelMendoza@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'Online Sales Associate'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1]
+        ],   
+        [
+          'user' => [
+            // 'prp_assigned' => 3,
+            // 'finance_assigned' => 4,
+            'first_name' => 'Jake Ellema',
+            'last_name' => 'Sasing',
+            'email' => 'JakeSasing@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'Utility/Messenger'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1]
+        ],
+        [
+          'user' => [
+            // 'prp_assigned' => 3,
+            // 'finance_assigned' => 4,
+            'first_name' => 'Mary Diane',
+            'last_name' => 'Salasayo',
+            'email' => 'MarySalasayo@bbo.com.ph',
+            'password' => Hash::make('123456789')
+          ],
+          'user_info' => [
+            'department_id' => 1
+            'shift_time_id'=> 1
+            'gender'=> 0
+            'company_number'=> '1236652'
+            // 'profile_url'=> null
+            'company_position'=> 'Marketng Staff'
+            'date_hired'=> '2018-3-6'
+            // 'contact_number'=> 
+            'civil_status'=> 'single'
+            'company_status'=> 'regular'
+            'birthday'=>'1984-4-13'
+            'address'=> 'Sta. Cruz, Liloan, Cebu'
+            // 'tin_number'=> 
+            // 'sss_number'=>
+            // 'pag_ibig_number'=>
+            // 'philhealth_number'=>
+            // 'allowed_leave_number'=>
+          ],
+          'user_roles' => [1]
+        ],
+    ];
+}
+
+
+// Marketing 
+// Atis, Cielo Fe Sasing
+// Gemarino, Dianen
+// Lapiz,Nina Rose Autida
+// Mendoza, Jocel Redotco
+// Sasing, Jake Ellema
+
+// Admin
+// Rosales, Fenella Corinne Real
+
+// Podador, April Claire Chagas
+
+// Hamoy, Gerard Winston Villegas
+
+// Web Development
+// Bacalla, Brisbane Jr Real
+
+
+
+
+// Finance
+// Salut,  Pretty Jean Mission
+
+// Mobile Development
+// Luta, Elbert Estardo
+// Navales, Joy Marie Lupague
+// Terdes, Nathaniel Cala
+
+// Web Development
+// Cabigas, Burt Karl Jusayan
+// Camporaso, Fellow James
+// Casipe, Cris Villaflores
+// Colina, Ivan Kirby Taghoy
+// Javier, Carl Wyner Velasco
+// Rosales, Jhonnel Bagayas
