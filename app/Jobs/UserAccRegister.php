05d256fa34c93d673aab1993bc5527d88a98ea67
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-13 23:36:09 +0800

    queing post-register emails

---
+++ app/Jobs/UserAccRegister.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Mail\Mailable;
+class UserAccRegister implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $mail;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Mailable $mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
