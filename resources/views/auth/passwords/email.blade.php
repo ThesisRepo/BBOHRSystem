76a7c7f5543292fb111b876cfe68e90037f659b2
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2020-11-13 23:18:29 +0800

    changes

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,12 +1,34 @@
 @extends('layouts.app')
+@section('style')
+    <link href="{{ asset('css/form.css') }}" rel="stylesheet">
+    <style>
+    .form-group button{
+        width:100%;
+        height:40px;
+    }
+    .form-group button[type=submit]{
+        background-color:#007bff;
+        color:white;
+        
+    }
+    .form-group button[type=button]{
+        background-color:#F4F6F6 ;
+        color:black;
+    }
+    </style>
+@endsection
+@section('js')
+   <script>
+   </script>
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8" style="margin-top:3%;">
+        <div class="col-md-9" style="margin-top:3%;">
             <div class="card">
-                <center>
-                    <img src="{{url('img/logoCircle.png')}}" width="100" alt="logo" style="margin-bottom:20px;">
+                <center style="margin-top: 3%;">
+                    <img src="{{url('img/logo.png')}}" width="150" height="100" alt="logo">
+                    <h4><b>Request to Reset</b></h4>
                 </center>
                 <div class="card-body">
                     @if (session('status'))
@@ -21,23 +43,42 @@
                     @endif
                     <form method="POST"  action="/forget-password">
                         @csrf
-
-                        <div class="form-group">
-                            <label for="email">Username</label><br>
-                            <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                name="email" value="{{ old('email') }}" placeholder="Enter your username" required autofocus>
-                            @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>Invalid Email or Password</strong>
-                            </span>
-                            @endif
+                        <div class="form-group  row" >
+                            <div class="col-md-10 mb-3  mx-auto" >
+                                <div class="md-form form-group">
+                                    <!-- <label for="email">Email</label><br> -->
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"  name="email"  value="{{ old('email') }}" required autocomplete="email" placeholder="Enter your email" autofocus>
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>Invalid Email or Password</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="form-group row mb-0" style="margin-top:5%;">
+                        <div class="form-group row">
+                            <div class="col-md-10  mx-auto">
+                                <button type="submit">
+                                   <strong>Send Link</strong> 
+                                </button>
+                            </div>
+                        </div>
+                        <br>
+                        <hr>
+                        <div class="form-group row">
+                            <div class="col-md-10  mx-auto">
+                                <center>
+                                    <strong style="color:blue; cursor: pointer;" onclick="window.location='{{ url("login") }}'"><b>Back to Login</b></strong>
+                                </center>
+                            </div>
+                        </div> 
+
+                        <!-- <div class="form-group row mb-0" style="margin-top:5%;">
                             <button type="submit" class="btn btn-primary">
                                 {{ __('Send Password Reset Link') }}
                             </button>
-                        </div>
+                        </div> -->
                     </form>
                 </div>
             </div>
