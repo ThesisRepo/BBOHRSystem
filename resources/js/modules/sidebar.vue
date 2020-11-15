00b29f5be047aa686f25d7c09463cbff6de7dcc3
unknown <paulinecalatrava@gmail.com>
2020-11-15 13:01:21 +0800

    add route for logout

--- resources/js/modules/sidebar.vue
+++ resources/js/modules/sidebar.vue
@@ -135,7 +135,7 @@ export default {
     logout() {
        this.$axios
         .post(
-          "http://bbomanagement.herokuapp.com/logout")
+          "https://bbomanagement.herokuapp.com/logout")
         .then(response => {
           location.reload();
           localStorage.clear()
