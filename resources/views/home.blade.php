64443f70c85a611ec5bf64feba34ea06189b57d3
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-15 23:14:07 +0800

    employee to cover relationship done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
 @auth
-<<<<<<< HEAD
-    <dashboard-view :user="{{auth()->user()->load('roles')->load('userInformation.department','userInformation.company_positions', 'assignedPrp', 'assignedFinance')}}"></dashboard-view>
-=======
     <dashboard-view :user="{{auth()->user()->load('roles')->load('userInformation.department','userInformation.company_positions','assignedPrp', 'assignedFinance')}}"></dashboard-view>
->>>>>>> 601d74ce67306c5d4c79804d1e85d6c8b260bcc7
 @endauth
 
 @endsection
