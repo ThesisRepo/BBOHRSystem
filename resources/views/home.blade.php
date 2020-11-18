985355da054ee13f917bf5a92ee29ef6b690ea90
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-18 23:21:55 +0800

    tshadang

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
-
+@section('js')
+<script>
+    localStorage.setItem('isloggedin', true)
+</script>
+@endsection
 @section('content')
 @auth
     <dashboard-view :user="{{auth()->user()->load('roles')->load('userInformation.department','userInformation.company_positions','assignedPrp', 'assignedFinance')}}"></dashboard-view>
+   
 @endauth
 
 @endsection
