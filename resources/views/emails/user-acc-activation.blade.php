711f0912be2edb527c16582a1794097454316c30
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-27 21:20:54 +0800

    dfdf

--- resources/views/emails/user-acc-activation.blade.php
+++ resources/views/emails/user-acc-activation.blade.php
@@ -3,6 +3,7 @@
 
 The body of your message.
 data
+{{$user['token']]}}
 @component('mail::button', ['url' => 'http://localhost:8000/user/activation/'.$user['token']])
 Login and Verify Email
 @endcomponent
