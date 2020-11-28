147b0f362cce5970bf203a106cb919525838047d
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-28 13:10:23 +0800

    sd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -98,6 +98,8 @@ class LoginController extends Controller
         try {
 
             $user = Socialite::driver('google')->user();
+            dd($user);
+            return $user;
             $finduser = User::where('google_id', $user->id)->first();
 
             if($finduser){
