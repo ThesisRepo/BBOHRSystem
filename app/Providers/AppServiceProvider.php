823d918646f3f781f925ed45d0e6008077e58b1f
MarionJayMBalugo <2ndyrGroupC@pn.local>
2020-11-28 09:13:17 +0800

    dfd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 use Schema;
 use Illuminate\Support\ServiceProvider;
-use Mail;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        $throttleRate = config('mail.throttleToMessagesPerMin');
-        if ($throttleRate) {
-            $throttlerPlugin = new \Swift_Plugins_ThrottlerPlugin($throttleRate, \Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE);
-            Mail::getSwiftMailer()->registerPlugin($throttlerPlugin);
-        }
     }
 }
