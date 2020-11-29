3e83e0edb6b2a197031f4ec6a2e2753fb7d684fe
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2020-11-29 23:41:19 +0800

    new update

--- resources/js/components/modals/create_leave.vue
+++ resources/js/components/modals/create_leave.vue
@@ -34,7 +34,7 @@
                 <v-tab @click="half = false, whole = true">Whole Day</v-tab>
                 <v-tab @click="whole = false, half = true">Half Day</v-tab>
             </v-tabs>
-            <v-row v-if="whole">
+            <v-row>
               <v-col cols="12">
                 <v-select
                   :items="leaveType"
@@ -45,7 +45,7 @@
                   required
                 ></v-select>
               </v-col>
-              <v-col cols="12" sm="4">
+              <v-col cols="12" sm="4" v-if="whole">
                 <v-menu
                   :close-on-content-click="true"
                   transition="scale-transition"
@@ -68,11 +68,10 @@
                     no-title
                     scrollable
                     color="primary"
-                    @change="changeDate()"
                   ></v-date-picker>
                 </v-menu>
               </v-col>
-              <v-col cols="12" sm="4">
+              <v-col cols="12" sm="4" v-if="whole">
                 <v-menu
                   :close-on-content-click="true"
                   transition="scale-transition"
@@ -105,7 +104,7 @@
                   style="color: red; font-size: 13px"
                 >Higher the End Date</span>
               </v-col>
-              <v-col cols="12" sm="4">
+              <v-col cols="12" sm="4" v-if="whole">
                 <v-text-field
                   label="Total Day/s of Leave*"
                   type="text"
@@ -114,25 +113,35 @@
                 ></v-text-field>
               </v-col>
             </v-row>
-            <v-row v-if="half">
-              <v-col cols="12">
-                <v-select
-                  :items="leaveType"
-                  label="Reason*"
-                  v-model="selectedLeaveType"
-                  item-text="name"
-                  item-value="value"
-                  required
-                ></v-select>
+            <v-row>
+              <v-col cols="6" v-if="half">
+                <v-menu
+                  :close-on-content-click="true"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="start_date"
+                      label="Start Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="start_date"
+                    :allowed-dates="disabledDates"
+                    no-title
+                    scrollable
+                    color="primary"
+                    @change="changeDate()"
+                  ></v-date-picker>
+                </v-menu>
               </v-col>
-              <v-col cols="4">
-                <v-select
-                  label="Date*"
-                  v-model="end_time"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="4">
+              <v-col cols="6" v-if="half">
                 <v-select
                   :items="halfSched"
                   label="Start Time*"
@@ -141,14 +150,6 @@
                   item-value="value"
                   required
                 ></v-select>
-              </v-col>
-              <v-col cols="4">
-                <v-text-field
-                  label="Total Day/s of Leave*"
-                  type="text"
-                  v-model="half_days_with_text"
-                  disabled
-                ></v-text-field>
               </v-col>
             </v-row>
           </v-container>
