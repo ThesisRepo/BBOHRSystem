88e33c419e37e7dd2a92a64e63ca2a3c18aa8e9e
Your Name <you@example.com>
2020-12-18 21:04:46 +0800

    lg

---
+++ resources/js/components/modals/confirmation/success.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-center">
+    <v-snackbar
+      v-model="snackbar"
+      :multi-line="multiLine"
+    >
+      {{ messages }}
+
+    <template v-slot:action="{ attrs }">
+        <v-btn
+          color="red"
+          class="white--text"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+    <!-- <v-btn color="red" class="white--text" outlined @click="hideModal()">Close</v-btn> -->
+    </template>
+    </v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      multiLine: true,        
+      snackbar: false
+    };
+  },
+  props: ['messages'],
+  methods: {
+    show(){
+      this.snackbar = true
+    },
+    hideModal(){
+      this.snackbar = false
+    }
+  }
+};
+</script>
