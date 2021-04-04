db32342915f09d67e08d753e5f0f99ca6d5aa6eb
Marion <lollipopskies02@gmail.com>
2021-04-04 19:28:14 +0800

    sd

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -104,7 +104,17 @@ export default {
             this.$store.commit('notificationAdd', {data:notif});       
             this.browserPopUp(notif);
           })
-
+        if(this.$store.getters.roleList.includes("admin")) {
+          console.log('ad');
+          Echo.private('*')
+          .listen('NewRequest', notif => {
+            this.$store.commit('notificationCountAdd');
+            this.$store.commit('notificationAdd', {data:notif});       
+            this.browserPopUp(notif);
+          })
+        }
+        
+        
         Echo.channel('newcalendarevent')
           .listen('NewCalendarEvent', notif => {
             this.$store.commit('notificationCountAdd');
