f43060c0ef5ce2b7887b0a460fb3e64ff4ae2725
Marion <lollipopskies02@gmail.com>
2021-04-04 15:53:00 +0800

    s

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,7 +35,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '45b01246e420b2fc4935',
-    cluster: 'ap1',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: false
 });
